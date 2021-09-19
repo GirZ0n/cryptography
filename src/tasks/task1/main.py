7b1d38ac9500b82bc92ad908f54212c7a665cc76
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-09-19 18:28:37 +0300

    Added kasiski test

---
+++ src/tasks/task1/main.py
@@ -0,0 +1,41 @@
+from collections import Counter
+from math import floor, sqrt
+from typing import List, Set, Tuple
+
+
+def get_lengths(ciphertext: str, segment_length: int):
+    # TODO: Могут ли пересекаться?
+    lengths = []
+    for i in range(len(ciphertext) - segment_length + 1):
+        for j in range(i + 1, len(ciphertext) - segment_length + 1):
+            if ciphertext[i : i + segment_length] == ciphertext[j : j + segment_length]:
+                lengths.append(j - i)
+    return lengths
+
+
+def get_divisors(number: int) -> Set[int]:
+    divisors = set()
+    for i in range(1, floor(sqrt(number) + 1)):
+        if number % i == 0:
+            divisors.add(i)
+            divisors.add(number // i)
+    return divisors
+
+
+def find_possible_key_lengths(ciphertext: str, segment_length: int = 2, top: int = 10) -> List[Tuple[int, int]]:
+    lengths = get_lengths(ciphertext, segment_length)
+
+    counter = Counter()
+    for length in lengths:
+        divisors = get_divisors(length)
+        counter.update(divisors)
+
+    return counter.most_common(top)
+
+
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        possible_key_lengths = find_possible_key_lengths(f.read(), 3)
+
+    for key_length, number in possible_key_lengths:
+        print(f'{key_length}: {number}')
