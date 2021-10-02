a1511ba41ecbd4a605ca84473db75aab9e3aab95
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-10-02 21:30:48 +0300

    Added decode

--- src/tasks/task2/main.py
+++ src/tasks/task2/main.py
@@ -1,10 +1,10 @@
 from bitarray import bitarray
-from bitarray.util import ba2hex
 
 from src.tasks.task2.config import (
     FILL_SYMBOL,
     INITIAL_PERMUTATION,
-    KEY_INITIAL_PERMUTATION, REVERSED_INITIAL_PERMUTATION,
+    KEY_INITIAL_PERMUTATION,
+    REVERSED_INITIAL_PERMUTATION,
 )
 from src.tasks.task2.utils.bitarray_utils import (
     convert_key_to_bitarray,
@@ -16,7 +16,7 @@ from src.tasks.task2.utils.feistel import feistel_transformations
 from src.tasks.task2.utils.key_utils import add_additional_bits, generate_subkeys
 
 
-def encode(text: str, key: str) -> str:
+def encode(text: str, key: str) -> bitarray:
     bitstring = convert_text_to_bitarray(text)
     blocks = split_by_block_length(bitstring, 64, FILL_SYMBOL)
     bitkey = convert_key_to_bitarray(key, number_of_bits=56, base=16)
@@ -30,8 +30,27 @@ def encode(text: str, key: str) -> str:
         encrypted_block = feistel_transformations(shuffled_block, subkeys)
         encrypted_text += permute(encrypted_block, REVERSED_INITIAL_PERMUTATION)
 
-    return ba2hex(encrypted_text)
+    return encrypted_text
+
+
+def decode(bitstring: bitarray, key: str) -> str:
+    blocks = split_by_block_length(bitstring, 64, FILL_SYMBOL)
+    bitkey = convert_key_to_bitarray(key, number_of_bits=56, base=16)
+    extended_key = add_additional_bits(bitkey)
+    shuffled_key = permute(extended_key, KEY_INITIAL_PERMUTATION)
+    subkeys = generate_subkeys(shuffled_key)
+
+    encrypted_text = bitarray()
+    for block in blocks:
+        shuffled_block = permute(block, INITIAL_PERMUTATION)
+        encrypted_block = feistel_transformations(shuffled_block, subkeys[::-1])
+        encrypted_text += permute(encrypted_block, REVERSED_INITIAL_PERMUTATION)
+
+    return encrypted_text.tobytes().decode()
 
 
 if __name__ == '__main__':
-    print(encode('Hello', 'ac43d5e3baf18e'))
+    key = '0000000000000F'
+    res = encode('Привет, меня зовут Илья!', key)
+    print(res)
+    print(decode(res, key))
